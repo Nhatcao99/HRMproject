e5e9a7122495321a25f4ee36700545e4559e6e07
Nhatcao99 <caominhnhat99@gmail.com>
2019-05-13 01:55:10 +0700

    change ChangePasswordprocess

--- VisualStudioCodeStuff/Common/ChangePassWordprocess.php
+++ VisualStudioCodeStuff/Common/ChangePassWordprocess.php
@@ -1,8 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Change Password</title>
+</head>
+<body>
 <?php
     $conn = new mysqli("localhost" , "root" , "" , "HRM");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    } 
+    }
+    $answer = "";
     $emCode = $_POST["EmCode"];
     $oldPass = $_POST["OldPass"];
     $newPass = $_POST["NewPass"];
@@ -13,17 +23,33 @@
         $row = mysqli_fetch_assoc($result);
         if ($oldPass == $row["password"])
         {
-                $sql1 = "UPDATE `Login` SET `password`= '$newPass' WHERE `EmployeeNumber` = '$emCode'";
-                if (mysqli_query($conn, $sql1)) {
-                    echo "Record Successful";
-                } else {
-                    echo "Error happen";
-                }
+            $sql1 = "UPDATE `Login` SET `password`= '$newPass' WHERE `EmployeeNumber` = '$emCode'";
+            if (mysqli_query($conn, $sql1)) {
+                $answer = "Record Successful";
+            } else {
+                $answer = "Error happen";
+            }
             
         } else{
-            echo "Your Password isn't true";
+            $answer = "Your Password isn't true";
         }
     } else{
-        echo "YOUR CODE DOESN'T EXIST";
+        $answer = "YOUR CODE DOESN'T EXIST";
     }
- ?>
+    ?>
+<h1 id = "title"><?php echo $answer ?></h1>
+<style>
+body{
+    background-image: URL("paper.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+#title {
+text-align: center;
+border: 3px solid green;
+padding: 70px 0;
+background-color: rgba(50, 115, 220, 0.3);
+}
+</style>
+</body>
+</html>
